30dcceb910a43edb29f8312635fee290690c75de
jck69 <jck69@pitt.edu>
2018-12-04 12:26:06 -0500

    Polished code and added striding and trimming

--- python/_Utils.py
+++ python/_Utils.py
@@ -1,10 +1,7 @@
 import os
 import glob
 
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure, draw
 import numpy as np
-
 import MDAnalysis
 from MDAnalysis.analysis import align
 
@@ -42,43 +39,6 @@ def del_filename(filename):
     # Deletes the filename with path filename.
     print "Deleting file: " + filename
     os.unlink(filename)
-
-
-def find_closest_points(data, codebook, labels):
-    # This function takes in a codebook and labels array and identifies the
-    # points closest to the codebook values within data. It returns an array
-    # of the indices of the points in data and distances from closest cluster
-    # center, as well as an array of the points themselves. Labels must be the
-    # same length as data, and codebook must have the same width as data.
-
-    # Prepare arrays for storing results.
-    closest_frames = np.zeros((len(codebook), 2))
-    closest_points = np.ndarray(closest_frames.shape)
-
-    # Set the first frame as the closest point.
-    for clust_index in range(0, len(codebook)):
-        closest_frames[clust_index,1] = np.sqrt(
-                            np.square(data[0,0] - codebook[clust_index,0]) + \
-                            np.square(data[0,1] - codebook[clust_index,1]))
-    
-    # For every frame, find its distance from the closest codebook value and
-    # keep track of the closest one
-    for index, clust_index in enumerate(labels):
-        dist = np.sqrt(np.square(data[index,0] - codebook[clust_index,0]) + \
-                       np.square(data[index,1] - codebook[clust_index,1]))
-        if dist < closest_frames[clust_index,1]:
-            closest_frames[clust_index,0] = index
-            closest_frames[clust_index,1] = dist
-            closest_points[clust_index] = data[index]
-    return closest_frames, closest_points
-
-
-def begin_plot(title, ylabel, xlabel, figure_number=1):
-    # This functions prepares plots with a title and axes labels.
-    plt.figure(figure_number,figsize=[10,7])
-    plt.title(title)
-    plt.ylabel(ylabel)
-    plt.xlabel(xlabel)
 
 
 def find_top_and_coor_files():
@@ -156,72 +116,29 @@ def load_traj(top_file, coor_file=None, align=True,
     return traj
 
 
-def check_axes(x_min, x_max, y_min, y_max):
-    # Makes sure that if either x_min or x_max is specified,
-    # the other is as well. Also checks y_min and y_max.
-    # Inform user if one axis is specified while the other is not.
-    # Make sure min values are less than max values.
-    if (x_min is not None and x_max is None) or \
-       (x_min is None and x_max is not None):
-        raise ValueError('ERROR: If you specify --axis_x_min, you must also specify ',
-                    '--axis_x_max, and visa versa.')
-    if (y_min is not None and y_max is None) or \
-       (y_min is None and y_max is not None):
-        raise ValueError('ERROR: If you specify --axis_y_min, you must also specify ',
-                    '--axis_y_max, and visa versa.')
-    if y_min is not None:
-        if y_min >= y_max:
-            raise ValueError('ERROR: Please make sure your --axis_y_max value is larger ',
-                            'than your --axis_y_min value.')
-    if x_min is not None:
-        if x_min >= x_max:
-            raise ValueError('ERROR: Please make sure your --axis_x_max value is larger ',
-                            'than your --axis_x_min value.')
+def compress_list(to_round, num_decimals):
+    # This function compresses a list by rounding its elements and
+    # losslessly converting them to integers
+    rounded_list = np.around(to_round, decimals=num_decimals)
+    multiplication_factor = 10 ** num_decimals
+    compressed_list = [x * multiplication_factor for x in rounded_list]
+    return compressed_list
 
+# Prunes trajectory to a given number of frames.
+# Trims the beginning of a trajectory keeping a given percentage.
+def stride_and_trim_traj(traj, stride, start_frame, ps_timestep):
+    
+    # Make sure we are affecting every atom in the trajectory.
+    sel = traj.select_atoms("all")
 
-def _save_dcd(traj, filename, sel_str):
-    # This function saves a MDAnalysis universe, or a subset of atoms
-    # therein, as a new file.
+    # Find number of frames in trajectory
+    num_frames = len(traj.trajectory)
 
-    sel = traj.select_atoms(sel_str)
-    with MDAnalysis.Writer(filename,
-                           multiframe=True,
-                           n_atoms=traj.atoms.n_atoms) as W:
-        for ts in traj.trajectory:
-            W.write(sel)
+    # Prune and trim as necessary. This also transfers the trajectory into memory.
+        traj.transfer_to_memory(start, num_frames, stride)
 
+    # Important for further RMSD analysis.
+    print("If old dt was " + str(ps_timestep) + " ps, new dt is " + \
+          str((ps_timestep * stride)) + " ps.")
 
-def save_pdb_first_frame(traj, filename, sel_str):
-    # This function saves the first frame of a MDAnalysis universe,
-    # or a subset of atoms therein, as a new file.
-
-    sel = traj.select_atoms(sel_str)
-    with MDAnalysis.Writer(filename,
-                           multiframe=True,
-                           n_atoms=traj.atoms.n_atoms) as W:
-        traj.trajectory[0]
-        W.write(sel)
-
-
-def align_trajectory(traj, sel_str="name CA", pdb_filename=None):
-    # This function aligns a trajectory to its first frame.
-
-    # If we are given a pdb to align to, load it as a universe
-    # and align to it.
-    if pdb_filename is not None:
-        ref = MDAnalysis.Universe(pdb_filename)
-        sel_ref = ref.select_atoms(sel_str)
-        alignment = align.AlignTraj(traj, ref, in_memory=True, select=sel_str)
-    else:
-        # Get the reference (first frame).
-        traj.trajectory[0]
-        
-        # Align the trajectory to current frame of trajectory
-        # (in this case the first). Note that this is in memory.
-        # Could run into problems with big trajectories...
-        alignment = align.AlignTraj(traj, traj, in_memory=True, select=sel_str)
-        alignment.run()
-
-    # Aligned trajectory is in traj.
     return traj
-
