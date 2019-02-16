a12395eb293062dd5f74358355bdc2eb0502070d
Jacob Durrant <jdurrant@MacBook-Pro.fios-router.home>
2019-02-15 21:32:33 -0500

    Updates.

---
+++ javascript/compile.sh
@@ -0,0 +1,26 @@
+# Clean old files
+rm BrowserSim.js BrowserSim.min.js closure.errs.txt
+
+# Typescript compile
+echo "Compile typescript."
+tsc --target ES5 BrowserSim.ts
+
+# Closure compile
+echo "Closure compile."
+# export formatting="--formatting=PRETTY_PRINT"
+export formatting=""
+
+# java -jar utils/closure-compiler-v20180506.jar $formatting \
+#    --compilation_level=ADVANCED_OPTIMIZATIONS --externs='utils/jquery-1.9.js' \
+#    --externs='utils/custom_extern.js' --js_output_file='BrowserSim.min.js' 'BrowserSim.js' \
+#    2> closure.errs.txt
+
+cp BrowserSim.js BrowserSim.min.js
+
+# Make example files
+echo "Make example files."
+cd examples
+python make.py
+
+echo "Start server."
+python -m SimpleHTTPServer 8000
