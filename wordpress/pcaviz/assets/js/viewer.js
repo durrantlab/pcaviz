115c3515ece587edd48ef8848ee5c5964a7d67ef
Jacob Durrant <jdurrant@MacBook-Pro.fios-router.home>
2019-06-28 21:23:04 -0400

    Autocenters on protein.

--- wordpress/pcaviz/assets/js/viewer.js
+++ wordpress/pcaviz/assets/js/viewer.js
@@ -1,30 +1,31 @@
 let element = jQuery('#viscontainer');
-        let config = {backgroundColor: 'white'};
-        let viewer = $3Dmol.createViewer(element, config);
+let config = {backgroundColor: 'white'};
+let viewer = $3Dmol.createViewer(element, config);
 
-        // Run sims in the browsers
-            function makeBrowserSim(viewer, viewerType,  datafile) {
-                element.show()
-                let browserSim = new BrowserSim({
-                    viewer: viewer,
-                    viewerType: viewerType,
-                    visStyle: {cartoon:{}, stick:{radius:.5,colorscheme:'Jmol'}},
-                    durationInMilliseconds: 10000,
-                    updateFreqInMilliseconds: 16.67,  // 60 fps
-                    loop: true,
-                    playerControlsID: document.getElementById('controls') ? 'controls' : element,
-                    windowAverageSize: 1,
-                });
+// Run sims in the browsers
+function makeBrowserSim(viewer, viewerType,  datafile) {
+    element.show()
+    let browserSim = new BrowserSim({
+        viewer: viewer,
+        viewerType: viewerType,
+        visStyle: {cartoon:{}, stick:{radius:.5,colorscheme:'Jmol'}},
+        durationInMilliseconds: 10000,
+        updateFreqInMilliseconds: 16.67,  // 60 fps
+        loop: true,
+        playerControlsID: document.getElementById('controls') ? 'controls' : element,
+        windowAverageSize: 1,
+    });
 
-                browserSim.io.loadJSON(datafile, () => {
-                    browserSim.player.start({
-                        durationInMilliseconds: 10000,
-                        updateFreqInMilliseconds: 16.67,  // 60 fps
-                        loop: true,
-                        windowAverageSize: 25
-                    });
+    browserSim.io.loadJSON(datafile, () => {
+        // Zoom in on the model.
+        viewer.zoomTo();
 
-                    window.browserSim = browserSim;
-                });
-            }
-
+        // Start playing.
+        browserSim.player.start({
+            durationInMilliseconds: 10000,
+            updateFreqInMilliseconds: 16.67,  // 60 fps
+            loop: true,
+            windowAverageSize: 25
+        });
+    });
+}
