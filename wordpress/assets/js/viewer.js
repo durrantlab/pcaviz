fe75cd3ad4c3bd659bcb918c60f6d6b59818923f
Jacob Durrant <jdurrant@MacBook-Pro.fios-router.home>
2019-07-22 13:55:50 -0400

    Added back wordpress directory.

---
+++ wordpress/assets/js/viewer.js
@@ -0,0 +1,31 @@
+let element = jQuery('#viscontainer');
+let config = {backgroundColor: 'white'};
+let viewer = $3Dmol.createViewer(element, config);
+
+// Run sims in the browsers
+function makeBrowserSim(viewer, viewerType,  datafile) {
+    element.show()
+    let pcaViz = new BrowserSim({
+        viewer: viewer,
+        viewerType: viewerType,
+        visStyle: {cartoon:{}, stick:{radius:.5,colorscheme:'Jmol'}},
+        durationInMilliseconds: 10000,
+        updateFreqInMilliseconds: 16.67,  // 60 fps
+        loop: true,
+        playerControlsID: document.getElementById('controls') ? 'controls' : element,
+        windowAverageSize: 1,
+    });
+
+    pcaViz.io.loadJSON(datafile, () => {
+        // Zoom in on the model.
+        viewer.zoomTo();
+
+        // Start playing.
+        pcaViz.player.start({
+            durationInMilliseconds: 10000,
+            updateFreqInMilliseconds: 16.67,  // 60 fps
+            loop: true,
+            windowAverageSize: 25
+        });
+    });
+}
