b10026218f530e837ec42d090925a89ab9b7d066
Jacob Durrant <jdurrant@MacBook-Pro.fios-router.home>
2019-07-22 14:06:36 -0400

    Update.

--- wordpress/compile_and_rsync_to_server.tmp.sh
+++
@@ -1,6 +0,0 @@
-cd ../javascript/
-./compile.sh
-cd -
-cp ../javascript/BrowserSim.min.js ./pcaviz/assets/js/BrowserSim.min.js
-rsync -rv pcaviz durrantj@durrantlab.pitt.edu:/var/www/html/wp-content/plugins/
-
